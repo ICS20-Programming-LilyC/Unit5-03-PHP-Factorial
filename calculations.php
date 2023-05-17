3dfe2afba081ebded97868fd7c39d0ba2164e280
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-05-17 00:28:11 +0000

    fixed code (need to change)

--- calculations.php
+++ calculations.php
@@ -1,37 +1,32 @@
 <?php
 
-// initializing variables
+//initializing variables
 $counter = 1;
 $factorialResult = 1;
 
-// getting user input for number
-$userNumber = $_POST['number'];
+//getting user input for number
+$userNum = $_POST['number'];
 
-// If: if user enters nothing or invalid input
-if (empty($userNumber) || !is_numeric($userNumber)) {
+//If: if user enters nothing
+if ((!is_numeric($userNum)) ) {
   echo "Please enter a valid number.";
 }
 
-// Else if: if user enters a negative number
-else if ($userNumber < 0) {
+//Else if: if user enters a negative number
+else if ($userNum < 0) {
   echo "Please enter a positive number.";
 }
 
-// Else if: if user enters 0
-else if ($userNumber == 0) {
-  echo "The factorial of 0 is 1.";
-}
-
-// Else: let the do-while loop execute to calculate the factorial
+//Else: let the do-while loop execute to calculate the factorial
 else {
-  // using a do...while loop to find factorial of inputted number
+  //using a do...while loop to find factorial of inputted number
   do {
-    $factorialResult *= $counter;
-    $counter++;
-  } while ($counter <= $userNumber);
+    $factorialResult = $factorialResult * $counter;
+    $counter = $counter + 1;
+  } while ($counter <= $userNum);
 
-  // displaying factorial to the screen
-  echo "The factorial of your inputted number (!" . $userNumber . ") is " . $factorialResult . ".";
+  //displaying factorial to the screen
+  echo "The factorial of your inputted number (!" . $userNum . ") = " . $factorialResult . ".";
 }
 
 ?>
